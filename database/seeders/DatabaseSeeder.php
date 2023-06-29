443a157ac0de5bf55a0faadbd2d0e2b40a555976
lacsapadnan <pascaladnan101@gmail.com>
2023-06-29 22:06:53 +0700

    Update seeder order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(WarehouseSeeder::class);
-        $this->call(UsersAndRolesSeeder::class);
         $this->call(PermissionSeeder::class);
+        $this->call(UsersAndRolesSeeder::class);
         $this->call(UnitSeeder::class);
         $this->call(TreasurySeeder::class);
     }
