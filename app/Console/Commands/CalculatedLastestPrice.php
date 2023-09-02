8244ab609291a6717825c2fc27ac50492e923646
lacsapadnan <pascaladnan101@gmail.com>
2023-09-02 09:29:57 +0700

    Update command calculated lastest price

--- app/Console/Commands/CalculatedLastestPrice.php
+++ app/Console/Commands/CalculatedLastestPrice.php
@@ -28,17 +28,17 @@ class CalculatedLastestPrice extends Command
     {
         $purchaseDetails = PurchaseDetail::with('product', 'unit')->get();
         foreach ($purchaseDetails as $purchaseDetail) {
-            if ($purchaseDetail->unit_id == $purchaseDetail->product->unit_dus && $purchaseDetail->product->dus_to_eceran != 0) {
+            if ($purchaseDetail->unit_id == $purchaseDetail->product->unit_dus && $purchaseDetail->product->dus_to_eceran) {
                 $purchaseDetail->product->update([
-                    'lastest_price_eceran' => $purchaseDetail->total_price / $purchaseDetail->product->dus_to_eceran
+                    'lastest_price_eceran' => $purchaseDetail->price_unit / $purchaseDetail->product->dus_to_eceran
                 ]);
-            } elseif ($purchaseDetail->unit_id == $purchaseDetail->product->unit_pak && $purchaseDetail->product->pak_to_eceran != 0) {
+            } elseif ($purchaseDetail->unit_id == $purchaseDetail->product->unit_pak && $purchaseDetail->product->pak_to_eceran) {
                 $purchaseDetail->product->update([
-                    'lastest_price_eceran' => $purchaseDetail->total_price / $purchaseDetail->product->pak_to_eceran
+                    'lastest_price_eceran' => $purchaseDetail->price_unit / $purchaseDetail->product->pak_to_eceran
                 ]);
             } elseif ($purchaseDetail->unit_id == $purchaseDetail->product->unit_eceran) {
                 $purchaseDetail->product->update([
-                    'lastest_price_eceran' => $purchaseDetail->total_price
+                    'lastest_price_eceran' => $purchaseDetail->price_unit
                 ]);
             }
         }
