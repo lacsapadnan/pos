c146fc481cca70ca00daaaccb3dfc19d2944b897
pascaladnan101@gmail.com <pascaladnan101@gmail.com>
2025-06-09 10:42:12 +0700

    feat: improve top products retrieval in dashboard by filtering out null products

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,13 +9,19 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        // Get more than 10 to account for possible null products, then filter
         $topProducts = SellDetail::select('product_id')
             ->selectRaw('SUM(quantity) as total_sold')
             ->groupBy('product_id')
             ->orderByDesc('total_sold')
             ->with('product')
+            ->take(20) // get more than needed to filter out nulls
+            ->get()
+            ->filter(function ($item) {
+                return $item->product !== null;
+            })
             ->take(10)
-            ->get();
+            ->values();
 
         return view('pages.dashboard', compact('topProducts'));
     }
