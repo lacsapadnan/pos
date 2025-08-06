99a8aa2483eb45020759b2a39dff51946c2999e6
pascaladnan101@gmail.com <pascaladnan101@gmail.com>
2025-08-06 14:12:22 +0700

    feat: add headings support to ProductExport for improved data export clarity

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Product;
 use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ProductExport implements FromCollection
+class ProductExport implements FromCollection, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -17,5 +18,21 @@ class ProductExport implements FromCollection
             Schema::getColumnListing('products'),
             ['created_at', 'updated_at']
         ))->get();
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        $columns = array_diff(
+            Schema::getColumnListing('products'),
+            ['created_at', 'updated_at']
+        );
+
+        // Convert column names to readable headers
+        return array_map(function ($column) {
+            return ucwords(str_replace('_', ' ', $column));
+        }, $columns);
     }
 }
